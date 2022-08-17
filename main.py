f8b306e11bed191b6b3a0498c2255b76975e434f
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-08-17 11:19:05 +0900

    [update] main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """main"""
 from logging import config
 
-from hoge.huga import Huga
-
 config.fileConfig("logging.conf", disable_existing_loggers=False)
 
+def main():
+    """main"""
+    print("hello world!")
+
 if __name__ == "__main__":
-    Huga().piyo()
+    main()
